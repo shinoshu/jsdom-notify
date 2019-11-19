6c7147054e5ed1e87c509173ee038f080e9a5769
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-11-19 17:00:02 +0900

    add tabletojson

--- index.js
+++ index.js
@@ -3,19 +3,29 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const jquery = require("jquery");
 const notifier = require('node-notifier');
+const tabletojson = require('tabletojson');
+const { Parser } = require('json2csv');
+const fs = require('fs');
 
-const url = 'https://github.com/shinoshu';
+const url = 'https://game8.jp/pokemon-sword-shield/260149';
 
 async function get() {
   try {
     const response = await axios.get(url);
-	const dom = new JSDOM(response.data);
-	const $ = jquery(dom.window);
-	const name = $('.user-status-message-wrapper')[1].textContent.trim();
-	notifier.notify(name);
+    const dom = new JSDOM(response.data);
+    const $ = jquery(dom.window);
+
+    const converted = tabletojson.convert(response.data);
+    // 取り出したいデータを選択
+    const data = converted[3];
+
+    const parser = new Parser();
+    const csv = parser.parse(data);
+
+    fs.writeFileSync('a.csv', csv);
   } catch (error) {
     console.error(error);
   }
 };
 
-get() && setInterval(get, 10 * 1000);
+get();
